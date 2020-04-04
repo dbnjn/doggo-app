880d60b72d28a0d08bebdfd715fbfbbd58cd12da
Shubham Badola <shubham.badola@byjus.com>
2020-04-04 18:15:35 +0530

    new

--- server/server.js
+++ server/server.js
@@ -14,7 +14,8 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "../client/build")))
 app.use(session({ secret: SESSION_SECRET, cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
 //Configure mongoose
