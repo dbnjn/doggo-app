d6a817e233af60421c0721dac539a3976b5ede1c
Shubham Badola <shubham.badola@byjus.com>
2020-04-04 19:07:37 +0530

    done

--- client/src/config/clientConfig.js
+++ client/src/config/clientConfig.js
@@ -1,5 +1,5 @@
 require("dotenv").config()
-const BASE_URI = `https://doggodebanjan.herokuapp.com:${process.env.PORT}/api`;
+const BASE_URI = `https://doggodebanjan.herokuapp.com/api`;
 const DOG_CEO_BASE_URI = "https://dog.ceo/api";
 
 export {
